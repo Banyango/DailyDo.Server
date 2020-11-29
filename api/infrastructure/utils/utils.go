339e76d9ae26c5902b9b65c705830c17149e540d
Kyle Reczek <kyle@banyango.com>
2020-11-29 14:21:36 -0700

    Better error stack traces

--- api/infrastructure/utils/utils.go
+++ api/infrastructure/utils/utils.go
@@ -3,9 +3,11 @@ package utils
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+	"runtime/debug"
 )
 
 func LogError(err error, code int, message string) error {
-	log.Printf("Error: %s,", err.Error())
+	log.Printf("Error: %s", err.Error())
+	debug.PrintStack()
 	return echo.NewHTTPError(code, message)
 }
